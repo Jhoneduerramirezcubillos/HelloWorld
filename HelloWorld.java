3dfca08b0c4fe8f27c0b6dbbf95a364a7174243d
Jhoneduerramirezcubillos <159643199+Jhoneduerramirezcubillos@users.noreply.github.com>
2024-02-10 13:20:21 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package HelloWorld; //linea 
+/**
+ *
+ * @author acer
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+        
+    }
+}
+
